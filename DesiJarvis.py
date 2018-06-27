0bcd37972e19cfa151c7af722a2fc547e6de18d8
Mayur Chhapra <mayurchhapra@gmail.com>
2018-06-28 00:09:34 +0530

    Main file[Script] added.

---
+++ DesiJarvis.py
@@ -0,0 +1,19 @@
+﻿# Developer: Mayur Navinbhai Chhapra
+# Purpos: Control Hardware of our PC by Python
+# Description: This script will effect your mouse and scroll 2px in every 7 minuts. 
+import pyautogui, sys
+import threading
+import time
+import datetime
+
+def autoPilotMode():
+	print("Mayur's DesiJarvish!")
+	print('દેશી જાર્વિસ, ઓટોપાયલટ મોડ ચાલુ')
+	for i in range(1,45):
+		time.sleep(60*7)
+		pyautogui.scroll(-500)
+		print("Jarvis Log: "+str(datetime.datetime.now().time()))
+try:
+	threading.Thread(target=autoPilotMode).start()
+except:
+	pass
